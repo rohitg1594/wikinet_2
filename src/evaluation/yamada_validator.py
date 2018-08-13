276746b37e9d974694dc7a965589270ce215e9d9
Rohit Gupta <rohitg1594@gmail.com>
2018-08-13 14:21:48 +0200

    fix call to cuda

--- src/evaluation/yamada_validator.py
+++ src/evaluation/yamada_validator.py
@@ -25,7 +25,7 @@ class YamadaValidator:
         for i in range(len(data)):
             data[i] = Variable(data[i])
 
-        if self.args.use_cuda:
+        if self.args.use_cuda and isinstance(self.args.device, int):
             for i in range(len(data)):
                 data[i] = data[i].cuda(self.args.device)
 
@@ -50,16 +50,4 @@ class YamadaValidator:
             total_mention += mention
 
         return total_correct, total_mention
-
-    def full_validate(self, model, dev_examples):
-        model = model.eval()
-
-        context_list = []
-        gold = []
-        for word_ids, examples in dev_data:
-            context_vec = normalize(word_embs[word_ids].mean(axis=0) @ orig_W + orig_b)
-            for mention, cands in examples:
-                gold.append(ent_dict[cands[0]])
-                context_list.append(context_vec)
-        context_matr = np.vstack(context_list)
 
