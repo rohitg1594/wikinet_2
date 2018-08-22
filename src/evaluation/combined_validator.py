1616872918d1c07700e1a89328f82bd85cda0cdf
Rohit Gupta <rohitg1594@gmail.com>
2018-08-22 14:56:07 +0200

    fix call to check errors

--- src/evaluation/combined_validator.py
+++ src/evaluation/combined_validator.py
@@ -369,11 +369,11 @@ class CombinedValidator:
         # Error analysis
         if error:
             print('Wiki Errors')
-            check_errors(I_wiki, self.wiki_all_gold[self.wiki_mask], self.wiki_mention_word_indices,
+            check_errors(I_wiki, self.wiki_all_gold[self.wiki_mask], self.wiki_mention_word_indices[self.wiki_mask, :],
                          self.rev_ent_dict, self.rev_word_dict, [1, 10])
             print('\n')
             print('Conll Errors')
-            check_errors(I_wiki, self.conll_all_gold, self.conll_mention_word_indices,
+            check_errors(I_conll, self.conll_all_gold, self.conll_mention_word_indices,
                          self.rev_ent_dict, self.rev_word_dict, [1, 10])
 
         return top1_wiki, top10_wiki, top100_wiki, mrr_wiki, top1_conll, top10_conll, top100_conll, mrr_conll
