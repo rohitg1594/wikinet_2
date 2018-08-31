f4f9dce9efb4b26307972d50c701a396a2989142
Rohit Gupta <rohitg1594@gmail.com>
2018-08-31 17:24:35 +0200

    change use_cuda type

--- src/combined_train.py
+++ src/combined_train.py
@@ -112,7 +112,7 @@ train_selection.add_argument('--train_linear', type=str2bool, help='whether to t
 
 # cuda
 parser.add_argument("--device", type=str, help="cuda device")
-parser.add_argument("--use_cuda", type=str, help="use gpu or not")
+parser.add_argument("--use_cuda", type=str2bool, help="use gpu or not")
 
 args = parser.parse_args()
 logger = get_logger(args)
