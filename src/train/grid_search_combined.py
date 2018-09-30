89b35bed6eb3db1025b20c41a446ebb0e3308592
Rohit Gupta <rohitg1594@gmail.com>
2018-09-30 19:45:41 +0200

    new grid search for with string

--- src/train/grid_search_combined.py
+++ src/train/grid_search_combined.py
@@ -17,8 +17,10 @@ np.warnings.filterwarnings('ignore')
 
 def grid_search():
     param_grid = {
-                  'lr': [0.01, 0.001, 0.005],
-                  'wd': [1e-5, 1e-6, 1e-7],
+                  'lr': [0.001, 0.005],
+                  'wd': [0, 1e-6, 1e-7],
+                  'gram_dim': [128, 256],
+                  'optim': ['adam', 'adagrad', 'rmsprop']
                   }
     results = {}
 
