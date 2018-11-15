2e9d3e88b878246804e2ecfbe7e95a9f1f119f5c
Rohit Gupta <rohitg1594@gmail.com>
2018-11-15 16:13:44 +0100

    fix numpy to tensor error

--- src/models/combined/full_context_string.py
+++ src/models/combined/full_context_string.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from src.models.combined.base import CombinedBase
 from src.models.combined.string_autoencoder import StringAutoEncoder
 from src.models.loss import Loss
-from src.utils.utils import numpy_to_tensor
+from src.utils.utils import np_to_tensor
 
 import numpy as np
 np.set_printoptions(threshold=10**8)
@@ -26,11 +26,11 @@ class FullContextString(CombinedBase, Loss):
 
         # Mention embeddings
         self.mention_word_embs = nn.Embedding(*mention_word_embs.shape, padding_idx=0, sparse=self.args.sparse)
-        self.mention_word_embs.weight.data.copy_(numpy_to_tensor(mention_word_embs))
+        self.mention_word_embs.weight.data.copy_(np_to_tensor(mention_word_embs))
 
         # Entity mention embeddings
         self.mention_ent_embs = nn.Embedding(*mention_ent_embs.shape, padding_idx=0, sparse=self.args.sparse)
-        self.mention_ent_embs.weight.data.copy_(numpy_to_tensor(mention_ent_embs))
+        self.mention_ent_embs.weight.data.copy_(np_to_tensor(mention_ent_embs))
 
         # Dropout
         self.dp = nn.Dropout(self.args.dp)
