26dbd6c27aa9f4b0da11e281498c36209084d07d
Rohit Gupta <rohitg1594@gmail.com>
2018-11-20 22:48:10 +0100

    fix

--- src/dataloaders/combined.py
+++ src/dataloaders/combined.py
@@ -86,7 +86,9 @@ class CombinedDataSet(object):
         # Candidate Char Tokens
         cand_char_cache_f_name = join(self.args.data_path, 'cache', f'cand_char_cache')
         if os.path.exists(cand_char_cache_f_name):
+            logger.info(f'Loading candidate char tokens dict.....')
             self.cand_char_dict = pickle_load(cand_char_cache_f_name)
+            logger.info(f'Candidate char tokens dict loaded.')
         else:
             logger.info(f'Creating candidate char tokens dict.....')
             self.cand_char_dict = {ent_str: self._get_char_tokens(ent_str) for ent_str in self.ent2id.keys()}
@@ -220,8 +222,7 @@ class CombinedDataSet(object):
         candidate_ids = self.candidate_dict[(ent_str, mention)]
         mention_char_tokens = self._get_char_tokens(mention)
         cand_strs = [self.id2ent.get(cand_id, ent_str) for cand_id in candidate_ids]
-        candidate_char_tokens = np.vstack([self.cand_char_dict.get(cand_str, self._get_char_tokens(cand_str))
-                                           for cand_str in cand_strs])
+        candidate_char_tokens = np.vstack([self.cand_char_dict.get(cand_str, self._get_char_tokens(cand_str)) for cand_str in cand_strs])
 
         output = {'mention_word_tokens': mention_word_tokens,
                   'mention_char_tokens': mention_char_tokens,
