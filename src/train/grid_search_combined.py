1030c0bb9baaeea4ae0a1211f111821f33d37f40
Rohit Gupta <rohitg1594@gmail.com>
2018-11-21 12:00:56 +0100

    change order of hyperparams

--- src/train/grid_search_combined.py
+++ src/train/grid_search_combined.py
@@ -20,7 +20,7 @@ DATA_TYPES = ['wiki', 'conll', 'msnbc', 'ace2004']
 
 def grid_search(**kwargs):
     param_grid = {
-                  'lr': [5e-2, 1e-3, 5e-3],
+                  'lr': [1e-3, 5e-2, 5e-3],
                   'wd': [1e-7, 1e-6, 1e-5],
                   'dp': [1e-1, 2e-1],
                   }
