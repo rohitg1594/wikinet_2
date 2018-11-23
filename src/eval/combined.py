38431f611c0f4c2cd1a9770613cab675abc6729a
Rohit Gupta <rohitg1594@gmail.com>
2018-11-23 17:45:10 +0100

    fix

--- src/eval/combined.py
+++ src/eval/combined.py
@@ -370,13 +370,17 @@ class CombinedValidator:
 
                     _, preds = index.search(mention_combined_embs.astype(np.float32), 100)
 
-                    print(preds[:50, :20])
-                    print(ent_combined_embs[0])
-                    
+                    # print(preds[:50, :20])
+                    # print(ent_combined_embs[0])
+
+                    if not np.any(preds[0]):
+                        logger.info("ALL PREDICTIONS ARE 0, IGNORING FIRST COLUMN")
+                        preds = preds[1:, :]
+
                     # Evaluate rankings
                     gold = self.numpy_data[data_type]['gold']
                     gold = gold[self.wiki_mask] if data_type == 'wiki' else gold
-                    top1, top10, top100, mrr = eval_ranking(preds, gold, [1, 10, 100])
+                    top1, top10, top100, mrr = eval_ranking(preds[1:, :], gold, [1, 10, 100])
                     results[data_type] = {'top1': top1,
                                           'top10': top10,
                                           'top100': top100,
