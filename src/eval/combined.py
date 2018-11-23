900c77fccf693759f82b2c411992ee27eee280c7
Rohit Gupta <rohitg1594@gmail.com>
2018-11-23 18:17:47 +0100

    fix

--- src/eval/combined.py
+++ src/eval/combined.py
@@ -371,7 +371,7 @@ class CombinedValidator:
 
                     _, preds = index.search(mention_combined_embs.astype(np.float32), 100)
 
-                    print(ent_combined_embs[0])
+                    # print(ent_combined_embs[0])
 
                     # print(preds[:50, :20])
                     # print(ent_combined_embs[0])
