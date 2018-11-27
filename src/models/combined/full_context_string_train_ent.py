57283fe099369edd3761a8b3888a852c031eeefb
Rohit Gupta <rohitg1594@gmail.com>
2018-11-27 21:11:49 +0100

    fix

--- src/models/combined/full_context_string_train_ent.py
+++ src/models/combined/full_context_string_train_ent.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 from logging import getLogger
+from os.path import join
 
 from src.models.combined.base import CombinedBase
 from src.models.combined.string_autoencoder import StringAutoEncoder
@@ -16,17 +17,20 @@ np.set_printoptions(threshold=10**8)
 logger = getLogger(__name__)
 
 
-class FullContextStringDan(CombinedBase, Loss):
+class FullContextStringTrainEnt(CombinedBase, Loss):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         # Unpack args
         mention_word_embs = kwargs['mention_word_embs']
-        mention_ent_embs = kwargs['mention_ent_embs']
         autoencoder_state_dict = kwargs['autoencoder_state_dict']
         hidden_size = kwargs['hidden_size']
         char_embs = kwargs['char_embs']
+        total_dims = self.args.mention_word_dim + self.args.context_word_dim + hidden_size
+        ent_embs = torch.load(join(self.args.data_path, 'ent_combined_embs.pickle'))
+
+        del self.ent_embs
 
         # Mention embeddings
         self.mention_word_embs = nn.Embedding(*mention_word_embs.shape, padding_idx=0, sparse=self.args.sparse)
@@ -34,9 +38,8 @@ class FullContextStringDan(CombinedBase, Loss):
         self.mention_word_embs.requires_grad = self.args.train_mention
 
         # Entity mention embeddings
-        self.mention_ent_embs = nn.Embedding(*mention_ent_embs.shape, padding_idx=0, sparse=self.args.sparse)
-        self.mention_ent_embs.weight.data.copy_(np_to_tensor(mention_ent_embs))
-        self.mention_ent_embs.requires_grad = self.args.train_mention
+        self.ent_embs = nn.Embedding(*ent_embs.shape, padding_idx=0, sparse=self.args.sparse)
+        self.ent_embs.weight.data.copy_(np_to_tensor(ent_embs))
 
         # Dropout
         self.dp = nn.Dropout(self.args.dp)
@@ -52,22 +55,8 @@ class FullContextStringDan(CombinedBase, Loss):
         self.autoencoder.requires_grad = False
 
         ##### DAN #######
-        total_dims = self.args.mention_word_dim + self.args.context_word_dim + hidden_size
-        init_func = getattr(nn.init, self.args.init_linear)
 
-        dan_layers = nn
-        for i in range(self.args.num_linear):
-            dan_layers.append(nn.Linear(total_dims, total_dims))
-            if i < self.args.num_linear - 1:
-                dan_layers.append(nn.ReLU())
-        dan_layers.append(nn.Sigmoid())
 
-        # for layer in dan_layers:
-        #     if isinstance(layer, nn.modules.linear.Linear):
-        #         init_func(layer.weight)
-
-        self.dan = nn.Sequential(*dan_layers)
-        print(self.dan)
 
     def forward(self, inputs):
         mention_word_tokens = inputs['mention_word_tokens']
@@ -78,13 +67,12 @@ class FullContextStringDan(CombinedBase, Loss):
 
         # Get string reps
         _, mention_str_rep, _ = self.autoencoder(mention_char_tokens)
-        _, candidate_str_rep, _ = self.autoencoder(candidate_char_tokens)
 
         # Get the embeddings
         mention_embs = self.dp(self.mention_word_embs(mention_word_tokens))
         context_embs = self.dp(self.word_embs(context_tokens))
-        candidate_mention_embs = self.dp(self.mention_ent_embs(candidate_ids))
-        candidate_context_embs = self.dp(self.ent_embs(candidate_ids))
+        candidate_embs = self.dp(self.ent_embs(candidate_ids))
+
 
         # Sum the embeddings over the small and large tokens dimension
         mention_embs_agg = torch.mean(mention_embs, dim=1)
@@ -92,18 +80,13 @@ class FullContextStringDan(CombinedBase, Loss):
 
         # Normalize
         mention_embs_agg = F.normalize(mention_embs_agg, dim=len(mention_embs_agg.shape) - 1)
-        candidate_mention_embs = F.normalize(candidate_mention_embs, dim=len(candidate_mention_embs.shape) - 1)
         context_embs_agg = F.normalize(context_embs_agg, dim=len(context_embs_agg.shape) - 1)
-        candidate_context_embs = F.normalize(candidate_context_embs, dim=len(candidate_context_embs.shape) - 1)
         mention_str_rep = F.normalize(mention_str_rep, dim=len(mention_str_rep.shape) - 1)
-        candidate_str_rep = F.normalize(candidate_str_rep, dim=len(candidate_str_rep.shape) - 1)
 
         # Cat the embs
         cat_dim = 2 if len(candidate_ids.shape) == 2 else 1
         mention_cat = torch.cat((mention_embs_agg, context_embs_agg, mention_str_rep), dim=1)
-        cand_cat = torch.cat((candidate_mention_embs, candidate_context_embs, candidate_str_rep), dim=cat_dim)
-        mention_repr = self.dan(mention_cat)
-        cand_repr = self.dan(cand_cat)
+
 
         # Dot product over last dimension only during training
         if len(candidate_ids.shape) == 2:
